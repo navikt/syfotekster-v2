254957363e276dbfb3e150a63e29bf9bd859a171
Martin Solheim <martin.solheim@me.com>
2019-06-11 16:50:50 +0200

    Oppdaterer deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,8 @@ docker build --build-arg version=latest --build-arg app_name=syfotekster-v2 -t "
 
 docker push "repo.adeo.no:5443/syfo/syfotekster-v2:$version"
 
-sed -e "s/{{VERSION}}/$version/g" nais.yaml > dist/nais.yaml
+sed -e "s/{{VERSION}}/$version/g" nais.yaml > dist-nais.yaml
 
-kubectl apply --context dev-fss --namespace default -f dist/nais.yaml
+kubectl apply --context dev-fss --namespace default -f dist-nais.yaml
 
-kubectl --context dev-sbs --namespace default rollout status -w deployment/syfotekster-v2
+kubectl --context dev-fss --namespace default rollout status -w deployment/syfotekster-v2
